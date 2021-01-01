ac791ed2d929961e78e6b3f885f94a1d910681c5
Victor Simiao <victor.sreis@hotmail.com>
2021-01-01 14:59:53 -0300

    Iniciando o projeto

---
+++ src/test/java/br/com/victorreis/crud/CrudUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorreis.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
