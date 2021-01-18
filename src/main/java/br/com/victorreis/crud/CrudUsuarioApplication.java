4b61b90e3ca0ff8750cad0c1f6b8989a73915339
Victor Simiao <victor.sreis@hotmail.com>
2021-01-18 13:41:35 -0300

    Documentando API com Swagger

--- src/main/java/br/com/victorreis/crud/CrudUsuarioApplication.java
+++ src/main/java/br/com/victorreis/crud/CrudUsuarioApplication.java
@@ -3,7 +3,10 @@ package br.com.victorreis.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CrudUsuarioApplication {
 
 	public static void main(String[] args) {
